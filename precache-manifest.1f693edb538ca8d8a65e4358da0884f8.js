a67bd90d49d563c5bb01e1e23a5732701c081d4e
AkibMankad <akibmankad555@gmail.com>
2022-09-18 18:46:36 +0530

    Updates

---
+++ precache-manifest.1f693edb538ca8d8a65e4358da0884f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5733ce6d639a9eb68205",
+    "url": "/TextUtilsReact/static/css/main.5fb10062.chunk.css"
+  },
+  {
+    "revision": "5733ce6d639a9eb68205",
+    "url": "/TextUtilsReact/static/js/main.5733ce6d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtilsReact/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtilsReact/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "bec29d1920facbe4fc02",
+    "url": "/TextUtilsReact/static/js/runtime~main.bec29d19.js"
+  },
+  {
+    "revision": "ff6fe65536d4405c838ae32096bac481",
+    "url": "/TextUtilsReact/index.html"
+  }
+];
